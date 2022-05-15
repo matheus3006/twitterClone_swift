403588e754460c523880660af457e5771f3ed214
matheus3006 <matheussouza18@live.com>
2022-05-15 12:02:32 -0300

    Initial Commit

---
+++ twitterClone/twitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitterCloneApp.swift
+//  twitterClone
+//
+//  Created by Matheus Souza on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct twitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
