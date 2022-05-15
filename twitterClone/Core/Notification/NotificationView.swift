a931f8ae7d949d25e26d501bc8f9f7c6d57a5593
matheus3006 <matheussouza18@live.com>
2022-05-15 20:38:49 -0300

    feat: add NotificationView

---
+++ twitterClone/Core/Notification/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  twitterClone
+//
+//  Created by Matheus Souza on 5/15/22.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Notification View")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
