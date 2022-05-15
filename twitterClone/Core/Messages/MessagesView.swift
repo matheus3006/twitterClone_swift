cf5dfcdccd9b6fbd7b2a556afdf491151f63a4e4
matheus3006 <matheussouza18@live.com>
2022-05-15 20:38:20 -0300

    feat: add MessageView

---
+++ twitterClone/Core/Messages/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesView.swift
+//  twitterClone
+//
+//  Created by Matheus Souza on 5/15/22.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text("MessagesView!")
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
