a577b9a0dc01b7cc8a2572441bb4c1b67aad3ad5
matheus3006 <matheussouza18@live.com>
2022-05-17 21:44:33 -0300

    feat: created ExplorerView

---
+++ twitterClone/Core/Explore/Views/ExploreView.swift
@@ -0,0 +1,39 @@
+//
+//  ExploreView.swift
+//  twitterClone
+//
+//  Created by Matheus Souza on 5/17/22.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationView{
+            VStack{
+                ScrollView{
+                    LazyVStack{
+                        ForEach(0 ... 25, id: \.self){ _ in
+                            NavigationLink {
+                                ProfileView()
+                            } label: {
+                                UserRowView()
+                            }
+
+                            
+                        }
+                    }
+                }
+                
+            }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
