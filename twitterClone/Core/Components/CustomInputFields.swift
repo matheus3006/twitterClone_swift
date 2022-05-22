1d0435484391399228e1de03b9c5205db2376521
matheus3006 <matheussouza18@live.com>
2022-05-22 16:24:23 -0300

    add: created CustomIputFields()

---
+++ twitterClone/Core/Components/CustomInputFields.swift
@@ -0,0 +1,40 @@
+//
+//  CustomInputFields.swift
+//  twitterClone
+//
+//  Created by Matheus Souza on 5/22/22.
+//
+
+import SwiftUI
+
+struct CustomInputFields: View {
+    let imageName: String
+    let placeHolderText: String
+    @Binding var text: String
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Image(systemName: imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(Color(.darkGray))
+                
+                TextField(placeHolderText, text: $text)
+            }
+            
+            Divider()
+                .background(Color(.darkGray))
+            
+            
+            
+        }
+    }
+}
+
+struct CustomInputFields_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomInputFields(imageName: "envelope", placeHolderText: "Email", text: .constant(""))
+    }
+}
