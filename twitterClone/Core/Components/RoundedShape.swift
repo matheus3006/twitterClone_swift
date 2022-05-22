b2a36923ca1d726505a730867434e9f21524e3ac
matheus3006 <matheussouza18@live.com>
2022-05-22 14:04:24 -0300

    feat:  created function to round all shapes ( RoundedShape )

---
+++ twitterClone/Core/Components/RoundedShape.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedShape.swift
+//  twitterClone
+//
+//  Created by Matheus Souza on 5/22/22.
+//
+
+import SwiftUI
+
+struct RoundedShape: Shape {
+    var corners: UIRectCorner
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: 80, height: 80))
+        
+        return Path(path.cgPath)
+    }
+}
+
+
