b18e4b3aae6bb1bf6defa20c08905b590ca253ef
matheus3006 <matheussouza18@live.com>
2022-05-22 18:56:16 -0300

    feat: add & implemented RegistrationView to project.

---
+++ twitterClone/Core/Authentication/Views/RegistrationView.swift
@@ -0,0 +1,93 @@
+//
+//  RegistrationView.swift
+//  twitterClone
+//
+//  Created by Matheus Souza on 5/22/22.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    @State private var email = ""
+    @State private var userName = ""
+    @State private var fullName = ""
+    @State private var password = ""
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack{
+            
+            //Header View
+            AuthHeaderView(
+                title1:"Get Started",
+                title2: "Create your account"
+            )
+            
+            VStack(spacing:40){
+                CustomInputFields(
+                    imageName: "envelope",
+                    placeHolderText: "Email",
+                    text: $email
+                )
+                
+                CustomInputFields(
+                     imageName: "person",
+                     placeHolderText: "UserName",
+                     text: $userName
+                )
+                CustomInputFields(
+                    imageName: "person",
+                    placeHolderText: "Full name",
+                    text: $fullName
+                )
+                
+                CustomInputFields(
+                     imageName: "lock",
+                     placeHolderText: "Password",
+                     text: $password
+                )
+                
+            }
+            .padding(32)
+            
+            Button {
+                print("Sign up here...")
+            } label: {
+                Text("Sign Up")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 340, height: 50)
+                    .background(Color(.systemBlue))
+                    .clipShape(Capsule())
+                    .padding()
+                
+            }
+            
+            Spacer()
+            
+            Button {
+                presentationMode.wrappedValue.dismiss()
+            } label: {
+                HStack{
+                    Text("Already have an account ")
+                        .font(.footnote)
+                    
+                    Text("Sign In")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                }
+            }
+            .padding(.bottom, 32)
+            
+
+            
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
--- twitterClone/twitterCloneApp.swift
+++ twitterClone/twitterCloneApp.swift
@@ -14,6 +14,7 @@ struct twitterCloneApp: App {
             NavigationView{
                 
                 ContentView()
+
                 
             }
         }
