e1332f589be60f5ee739aaaf7e334df4c473a321
matheus3006 <matheussouza18@live.com>
2022-05-22 21:24:11 -0300

    feat: add FireStore to project

--- twitterClone/twitterCloneApp.swift
+++ twitterClone/twitterCloneApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct twitterCloneApp: App {
+    init(){
+        FirebaseApp.configure()
+        
+    }
     var body: some Scene {
         WindowGroup {
             NavigationView{
