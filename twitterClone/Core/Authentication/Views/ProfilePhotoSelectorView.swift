46116e1d54254b6c0d8e46ae9eee268f88c6de82
matheus3006 <matheussouza18@live.com>
2022-05-26 21:38:37 -0300

    feat: add ProfilePhotoSelectorView

--- twitterClone/Core/Authentication/Views/ProfilePhotoSelectorView.swift
+++ twitterClone/Core/Authentication/Views/ProfilePhotoSelectorView.swift
@@ -7,31 +7,51 @@
 
 import SwiftUI
 
-struct ProfilePhotoSelectorView: View {
+struct ProfilePhotoSelectorView : View {
+    @State private var showImagePicker = false
+    @State private var selectedImage: UIImage?
+    @State private var profileImage : Image?
+    @EnvironmentObject var viewModel: AuthModel
+    
     var body: some View {
         VStack{
-            AuthHeaderView(title1: "Setup Account", title2: "Add a profile Photo")
+            AuthHeaderView(title1:"Setup Account",
+                           title2:"Add a profile Photo")
             
-            Button {
-                print("Pick image Here")
-            } label: {
-                VStack{
-                    Image(systemName: "plus.circle")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 175, height: 175)
-                        .padding(.top, 44)
-                    Text("PHOTO")
-                        .fontWeight(.semibold)
-                
+                        
+            imagePicker
+                .sheet(isPresented:$showImagePicker, onDismiss: loadImage){
+                    ImagePicker(selectedImage : $selectedImage)
                 }
+                .padding(.top,44)
+            
+            if let selectedImage = selectedImage {
+                Button{
+                    viewModel.uploadProfileImage(image: selectedImage)
+                }label:{
+                    Text("Continue")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width:340,height:50)
+                        .background(Color(.systemBlue))
+                        .clipShape(Capsule())
+                        .padding()
+                }
+                .shadow(color:.gray.opacity(0.5),radius:10,x:0,y:0)
             }
-
+            
             
             Spacer()
+            
         }
         .ignoresSafeArea()
     }
+    
+    func loadImage(){
+        guard let selectedImage=selectedImage else{return}
+        profileImage = Image(uiImage:selectedImage)
+    }
+    
 }
 
 struct ProfilePhotoSelectorView_Previews: PreviewProvider {
@@ -40,6 +60,44 @@ struct ProfilePhotoSelectorView_Previews: PreviewProvider {
     }
 }
 
+//MARK: - ImagePicker & Continue Button
+extension ProfilePhotoSelectorView {
+    var imagePicker: some View {
+        
+        Button{
+            showImagePicker.toggle()
+        }label:{
+            if let profileImage = profileImage {
+                if let profileImage = profileImage {
+                    profileImage
+                        .resizable()
+                        .modifier(ProfileImageModifier())
+                }
+            }else{
+                VStack{
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 175, height: 175)
+                    
+                    Text("PHOTO")
+                        .fontWeight(.semibold)
+                }
+                
+            }
+        }
+        
+    }
+    
+    
+}
 
-
-   
+private struct ProfileImageModifier : ViewModifier{
+    func body(content:Content)->some View{
+        content
+            .foregroundColor(Color(.systemBlue))
+            .scaledToFill()
+            .frame(width:180,height:180)
+            .clipShape(Circle())
+    }
+}
