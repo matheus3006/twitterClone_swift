3d78f85aee80ab2b2e77aba32f6b8cb793e6226c
matheus3006 <matheussouza18@live.com>
2022-05-27 22:20:40 -0300

    fix: add missing downloadURL on ImageUploader

--- twitterClone/Service/ImageUploader.swift
+++ twitterClone/Service/ImageUploader.swift
@@ -7,22 +7,31 @@
 
 import FirebaseStorage
 import UIKit
+import Firebase
 
 struct ImageUploader{
     static func uploadImage(image : UIImage,completion: @escaping(String)->Void){
-        guard let imageData = image.jpegData(compressionQuality:0.5) else {return}
+        guard let imageData = image.jpegData(compressionQuality:0.3) else {return}
         let filename = NSUUID().uuidString
         
-        let ref = Storage.storage().reference(withPath:"/profile_image/\(filename)")
-        ref.putData(imageData, metadata: nil){ _ , error in
+        let ref = Storage.storage().reference(withPath: "/profile_image/\(filename)")
+        ref.putData(imageData, metadata: nil) { _ , error in
             if let error=error{
                 print("DEBUG:Failed to upload image with error:\(error.localizedDescription)")
                 
                 return
                 
             }
-            
-            
+        
+            ref.downloadURL { imageUrl , error in
+                if let error=error{
+                    print("DEBUG:Failed get image url with error:\(error.localizedDescription)")
+                    return
+                }
+                guard let imageUrl=imageUrl?.absoluteString else{return}
+                completion(imageUrl)
+                
+            }
             
             
         }
