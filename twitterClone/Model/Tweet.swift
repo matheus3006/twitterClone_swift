5225f7fc6d76f0cae9dc29b0ff7ad3eabb030d54
matheus3006 <matheussouza18@live.com>
2022-05-28 22:34:21 -0300

    feat: add Tweet struct to handle Tweet format data coming from backend

---
+++ twitterClone/Model/Tweet.swift
@@ -0,0 +1,23 @@
+//
+//  Tweet.swift
+//  twitterClone
+//
+//  Created by Matheus Souza on 5/28/22.
+//
+
+import FirebaseFirestoreSwift
+import Firebase
+
+struct Tweet : Identifiable,Decodable {
+    @DocumentID var id:String?
+    
+    let caption:String
+    
+    let timestamp:Timestamp
+    
+    let uid:String
+    
+    var likes: Int
+    
+    var user: User?
+}
