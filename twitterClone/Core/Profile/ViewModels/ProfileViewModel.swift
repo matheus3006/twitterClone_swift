498c4979b8f85b7475dd8a31abd5d428b589544e
matheus3006 <matheussouza18@live.com>
2022-05-31 19:26:57 -0300

    feat: add actionButtonTitle

--- twitterClone/Core/Profile/ViewModels/ProfileViewModel.swift
+++ twitterClone/Core/Profile/ViewModels/ProfileViewModel.swift
@@ -10,7 +10,10 @@ import Foundation
 
 class ProfileViewModel:ObservableObject{
     @Published var tweets = [Tweet]()
+    @Published var likedTweets = [Tweet]()
+    
     private let service = TweetService()
+    private let userService = UserService()
     let user : User
     
     init(user:User){
@@ -18,7 +21,27 @@ class ProfileViewModel:ObservableObject{
         self.user = user
         self.fetchUserTweets()
     }
-     
+    
+    var actionButtonTitle:String{
+        return user.isCurrentUser  ? "Edit Profile":"Follow"
+    }
+                                  
+    
+    func tweets(forFilter filter:TweetFilterViewModel)->[Tweet]{
+        switch filter {
+        case .tweets:
+            return tweets
+            
+        case .replies:
+            return tweets
+            
+        case .likes:
+            return likedTweets
+        }
+        
+        
+        
+    }
     
     func fetchUserTweets(){
         guard let uid = user.id else{return}
@@ -31,4 +54,23 @@ class ProfileViewModel:ObservableObject{
             }
         }
     }
+    
+    func fetchLikedTweets(){
+        guard let uid = user.id else{return}
+        
+        service.fetchLikedTweets(forUid:uid){ tweets in
+            self.likedTweets = tweets
+            
+            
+            for i in 0 ..< tweets.count {
+                let uid=tweets[i].uid
+                self.userService.fetchUser(withUid:uid ){ user in
+                    self.likedTweets[i].user=user
+                }
+            }
+            
+        }
+        
+    }
+    
 }
